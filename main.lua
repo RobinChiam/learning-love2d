39c11abb7b6298420712ba7aabecb008d2243159
Robin Chiam <raresugarglider@gmail.com>
2026-01-18 22:35:38 +0800

    Player movement with Circle as a sprite

--- main.lua
+++ main.lua
@@ -1,3 +1,31 @@
+
+function love.load()
+    player = {}
+    player.x = 100
+    player.y = 100
+    player.speed = 5
+
+
+end
+
+function love.update(dt)
+
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+    if love.keyboard.isDown("up") then
+        player.y = player.y - player.speed
+    end
+    if love.keyboard.isDown("down") then
+        player.y = player.y + player.speed
+    end
+
+end
+
+
 function love.draw()
-    love.graphics.print("Hello World", 300, 300)
+    love.graphics.circle("fill",player.x, player.y, 10)
 end
